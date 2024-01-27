41fdadf5e47ec1393174f9cc3ba9e8b1bac379c7
MoeinPQ <moien.barsa@gmail.com>
2024-01-27 05:35:14 +0330

    test home page , it's not compeleted

--- HumanResource/src/App.jsx
+++ HumanResource/src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import EmployeeLogin from "./containers/Employee-login.jsx";
+import EmployeeHome from "./containers/Employee-home.jsx";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <EmployeeLogin />
+        <EmployeeHome />
     </>
   )
 }
