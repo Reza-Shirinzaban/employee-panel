9dceb28a8d3b32a71047d1cd2c98631e183ef67b
mohammad ghasemi <rezashirinzaban20@gmail.com>
2025-01-16 14:36:26 +0330

    update employee-login

--- HumanResource/src/containers/Employee-login.jsx
+++ HumanResource/src/containers/Employee-login.jsx
@@ -1,26 +1,17 @@
-import { useState } from "react";
 import EmployeeLoginForm from "../components/Employee-login-form.jsx";
 
-const EmployeeLogin = ({ setLogin }) => {
-  return (
-    <>
-      <div className="flex">
-        <div className="min-[1440px]:w-[720px] xl:w-[600px] lg:w-[450px] md:w-[150px] md:h-[1024px] md:bg-[#C09015]"></div>
-        <div className="flex flex-col md:pt-80 max-md:pt-48">
-          <p className="text-4xl font-bold pl-72 max-md:hidden">پنل کارمندان</p>
-          <p className="text-4xl font-bold md:pl-72 text-center md:hidden">
-            ورود به پنل
-          </p>
-          <div>
-            <EmployeeLoginForm setLogin={setLogin} />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+const EmployeeLogin = () => {
+
+    return (
+        <>
+            <div className="grid grid-cols-1 h-full sm:grid-cols-2">
+                <div className="bg-[#C09015] hidden sm:block"></div>
+                <div className="grid items-center">
+                    <EmployeeLoginForm/> {/* پاس دادن setLogin به فرم  setLogin={setLoginStatus}*/}
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default EmployeeLogin;
-// width: 720px;
-// height: 1024px;
-// flex-shrink: 0;
